b5546ada1cc600a75ccda953f496e2227306ebc9
Yuki Yamaura <ymruki@gmail.com>
2024-04-28 13:09:18 +0900

    Add comments to pages

--- text2pixel_pages/src/main.rs
+++ text2pixel_pages/src/main.rs
@@ -16,9 +16,15 @@ fn text2datapng(text: &str) -> String {
 fn App() -> impl IntoView {
     let (text, set_text) = create_signal(String::new());
     view! {
+        <div>
         <textarea on:keyup=move |e| { set_text(event_target_value(&e)) }></textarea>
             <div>
         <img src=move || text2datapng(&text.get())/>
+        </div>
+        <div>
+        <h2>text2pixel</h2>
+        <p>The input text is converted into a PNG image using pixel fonts. The conversion is confined within the browser environment, and no data is transmitted to the server.</p>
+        </div>
         </div>
     }
 }
